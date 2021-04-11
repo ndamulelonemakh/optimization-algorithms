14abb81acf353556257e44357cd43fed22a97af9
Ndamulelo Nemakhavhani <ndamulelo@bbd.co.za>
2021-04-11 13:54:18 +0200

    First gradient descent went well

--- gradientdescent.py
+++ gradientdescent.py
@@ -13,13 +13,25 @@
     where gk is the gradient vector at point Xk and A is the hessian matrix
 """
 import math
+import logging
 import numpy as np
+
+logging.basicConfig(level=logging.DEBUG)
+log = logging.getLogger(__name__)
 
 
 class GradientDescent:
-    def __init__(self, hessian_matrix=None, linear_terms=None):
+    def __init__(self, max_iterations=60, hessian_matrix=None, linear_terms=None):
         self.Hessian: np.ndarray = hessian_matrix or np.array([[10, 4], [4, 2]])
         self.LinearCoefficients = linear_terms or np.array([-14, -6])
+        self.Minimiser = np.array([0, 0])
+        self.CurrentIteration = 0
+        self.MaxIterations = max_iterations
+
+    @property
+    def __current_gradient_value(self):
+        gradient = self.del_f(self.Minimiser)
+        return np.sqrt(gradient.dot(gradient))
 
     @staticmethod
     def f(xk, decimal_places=3):
@@ -30,16 +42,38 @@ class GradientDescent:
 
     def del_f(self, xk: np.ndarray) -> np.ndarray:
         gradient_vector = np.matmul(self.Hessian, xk)
-        if self.LinearCoefficients:
+        if isinstance(self.LinearCoefficients, np.ndarray):
             gradient_vector = np.add(gradient_vector, self.LinearCoefficients)
         return gradient_vector
 
+    def exact_step_length(self, xk: np.ndarray, decimal_places=3) -> np.float64:
+        """Get the step lengh by minimising f(x + ad_k) wrt to a
 
+        alpha(or lambda) = || gk || ** 2 / gk_T * A * gk
+        """
+        gk = self.del_f(xk)
+        numerator = gk.dot(gk)
+        denominator = gk.dot(self.Hessian).dot(gk)
+        step_len = numerator/denominator
+        return round(step_len, decimal_places)
 
     def _find_next_candidate(self):
-        """X_k+1 = X_k + alpha * del_F(Xk)"""
-        pass
+        """Get next potential minimum using X_k+1 = X_k + alpha * del_F(Xk)"""
+        xk = self.Minimiser
+        dk = -1 * self.del_f(xk)
+        step_length = self.exact_step_length(xk)
+        x_k_plus1 = xk + (step_length * dk)
+        log.debug(f"X**{self.CurrentIteration} = {xk} - {step_length} * {dk}")
+        return x_k_plus1
 
     def execute(self):
-
-        print("Done")
+        log.info('Coordinate descent iteration started')
+        # self.__preview_config()
+        for k in range(self.MaxIterations):
+            log.debug(f"-----------Iteration {k}--------------")
+            self.CurrentIteration = k
+            self.Minimiser = self._find_next_candidate()
+            log.debug(f"------Minimiser = {self.Minimiser}. Gradient = {self.__current_gradient_value}--------\n")
+        log.info(f"------Minimiser = {self.Minimiser}. Gradient = {self.__current_gradient_value}--------")
+        log.info('Coordinate descent completed successfully')
+        return self.Minimiser, self.f(self.Minimiser), round(self.__current_gradient_value, 3)
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import coordinatedescent
+import gradientdescent
 
 
 def main():
-    optimizer = coordinatedescent.CoordinateDescent()
+    optimizer = gradientdescent.GradientDescent()
     minimiser, min_value, gradient = optimizer.execute()
     result = f'''
     =====Function F(X1, X2) has a local minimum at {minimiser}=========
